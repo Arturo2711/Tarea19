3d40d03bebcd154ec70482cec2c82d79ce33d53a
Ulises Gachuz <109196509+UGachuzD@users.noreply.github.com>
2023-06-26 00:42:25 -0600

    Cambios dinales Delfin

--- Gato.java
+++ Gato.java
@@ -1,3 +1,33 @@
-public class Gato extends Animal{
+public class Gato extends Animal implements IMascota{
+    private String nombre;
+
+    public Gato() {
+        super(4);
+    }
+
+    public Gato(String nombre) {
+        super(4);
+        this.nombre = nombre;
+    }
+
+    @Override
+    public void comer() {
+        System.out.println("El gato está comiendo.");
+    }
+
+    @Override
+    public String obtenerNombre() {
+        return this.nombre;
+    }
+
+    @Override
+    public void ponerNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public void jugar() {
+        System.out.println("El gato está jugando.");
+    }
     
 }

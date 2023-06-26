95832acf24efb630fec79f05bfa39ed2e98bfa7f
Arturo2711 <132433617+Arturo2711@users.noreply.github.com>
2023-06-26 01:07:09 -0600

    Create App.java

---
+++ App.java
@@ -0,0 +1,13 @@
+
+
+public class App {
+
+  public static void main(String[] args) {
+    Delfin midelfin = new Delfin("Carson");
+    Escarabajo miescarabajo = new Escarabajo(2, 10);
+    Gato mGato = new Gato("Larson");
+    midelfin.jugar();
+    miescarabajo.comer();
+    mGato.jugar();
+  }
+}

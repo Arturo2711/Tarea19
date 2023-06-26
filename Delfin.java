91ce5b2547e2ba65c95258a4b7ae3a2b38db1e16
Ulises Gachuz <109196509+UGachuzD@users.noreply.github.com>
2023-06-26 00:42:03 -0600

    Cambios finales Delfin

--- Delfin.java
+++ Delfin.java
@@ -1,3 +1,32 @@
-public class Delfin extends Animal, implements Mascota{
+public class Delfin extends Animal implements IMascota{
+    private String nombre;
 
+    public Delfin() {
+        super(0);
+    }
+
+    public Delfin(String nombre) {
+        super(0);
+        this.nombre = nombre;
+    }
+
+    @Override
+    public void comer() {
+        System.out.println("El delfín está comiendo.");
+    }
+
+    @Override
+    public String obtenerNombre() {
+        return this.nombre;
+    }
+
+    @Override
+    public void ponerNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public void jugar() {
+        System.out.println("El delfín está jugando.");
+    }
 }
